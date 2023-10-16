d04845327f4a95aa1c997a17dfc84be7172d4489
Santiago Miguez <smiguez85@gmail.com>
2023-10-16 15:22:19 +0200

    chore: remove unused output file

--- multipass/output.tf
+++
@@ -1,7 +0,0 @@
-output "master" {
-  value = join(",", data.external.master[*].result.name, data.external.masters[*].result.name)
-}
-
-output "workers" {
-  value = join(",", data.external.workers[*].result.name)
-}
